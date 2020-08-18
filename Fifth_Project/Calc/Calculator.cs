c530cfe57c5b4b1b946dce520c2310205b746268
FarjanaFerdousiTamanna <farjana.ferdousi.tamanna@gmail.com>
2020-08-18 13:54:08 +0600

    Add project files.

---
+++ Fifth_Project/Calc/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fifth_Project.Calc
+{
+    class Calculator
+    {
+        // creating a static method 
+        public static int Add(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Answer = x + y;
+            return Answer;
+
+        }
+    }
+}
---
+++ Fifth_Project/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Fifth_Project.Calc;
+
+namespace Fifth_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Result = Calculator.Add(5, 10);
+            Console.WriteLine(Result);
+        }
+    }
+}
